ad5870e1553329eca4a26489b6492e6477b26910
Cuneyd7 <cuneydkopuz@gmail.com>
2026-04-27 21:23:46 +0300

    Python IDLE uzerinden otomatik guncelleme

--- YKI_V1/yki_test_otomasyon.py
+++ YKI_V1/yki_test_otomasyon.py
@@ -10,7 +10,7 @@ class Color:
     BOLD = '\033[1m'
     END = '\033[0m'
 
-BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = "http://127.0.0.25:5000"
 session = requests.Session()
 
 def log_success(msg):
